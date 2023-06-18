fae83e6894250b52126e6c43a5fbfdb7ed542fbc
Skyfay <skyfay@skymail.one>
2023-06-18 17:05:19 +0200

    First Python Gui Setup

---
+++ Development/Powershell/software-2.0-test.ps1
@@ -0,0 +1,65 @@
+﻿cls
+# Script von Skyfay
+# Support auf Githup oder support@skyfay.ch
+
+# ------------------------------------------------------------------------------ #
+
+# AppData-Ordnerpfad holen
+$appDataPath = [Environment]::GetFolderPath("LocalApplicationData")
+
+# Ordnerpfad erstellen
+$folderPath = Join-Path $appDataPath "SkyfaySoftware\AutoNicConfigurator"
+
+# Ordner erstellen, falls er noch nicht vorhanden ist
+if (-not (Test-Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath | Out-Null
+}
+
+# JSON-Dateipfad erstellen
+$jsonFilePath = Join-Path $folderPath "db.json"
+
+# JSON-Datei erstellen, falls sie noch nicht vorhanden ist
+if (-not (Test-Path $jsonFilePath)) {
+    $jsonObject = @{
+        "Network-Adapter" = @()
+    } | ConvertTo-Json -Depth 100
+    Set-Content $jsonFilePath $jsonObject
+}
+
+# ------------------------------------------------------------------------------ #
+
+# JSON-Datei laden
+$jsonObject = Get-Content $jsonFilePath | ConvertFrom-Json
+
+# Misst die Anzahl der Netzwerk Adapter, welche auf dem System vorhanden sind
+$netAdapterCount = Get-NetAdapter | Measure
+
+# Netzwerkadapter zählen und Informationen in ein Array speichern
+$netAdapters = Get-NetAdapter | Select-Object Name, Status, MacAddress, LinkSpeed
+$netAdapterCount = $netAdapters.Count
+
+# Array für die Adapter erstellen
+$adapters = @()
+
+# Informationen zu jedem Adapter hinzufügen und in das Array einfügen
+foreach ($adapter in $netAdapters) {
+    $adapterInfo = @{
+        "Name" = $adapter.Name
+        "Status" = $adapter.Status
+        "MacAddress" = $adapter.MacAddress
+        "Speed" = $adapter.LinkSpeed
+    }
+    $adapters += $adapterInfo
+}
+
+# JSON-Objekt aktualisieren
+$jsonObject."Network-Adapter" = @{
+    "Count" = $netAdapterCount
+    "Adapters" = $adapters
+}
+
+# JSON-Datei aktualisieren
+$jsonObject | ConvertTo-Json -Depth 10 | Set-Content $jsonFilePath
+
+
+# ------------------------------------------------------------------------------ #
---
+++ Development/Python/network.py
@@ -0,0 +1,48 @@
+import json
+import os
+import psutil
+import socket
+
+def get_network_adapters_info():
+    adapters = []
+
+    # Get network adapters information using psutil module
+    for interface, addresses in psutil.net_if_addrs().items():
+        adapter_info = {}
+        adapter_info['name'] = interface
+
+        # Get IPv4 address, subnet mask, and gateway
+        for address in addresses:
+            if address.family == socket.AF_INET:
+                adapter_info['ip'] = address.address
+                adapter_info['subnet_mask'] = address.netmask
+                adapter_info['gateway'] = address.broadcast
+
+        # Get DNS servers using socket module
+        dns_servers = []
+        try:
+            resolver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            resolver.connect(('8.8.8.8', 80))
+            dns_servers.append(resolver.getsockname()[0])
+            resolver.close()
+        except socket.error:
+            pass
+        adapter_info['dns_servers'] = dns_servers
+
+        # Get network speed information using psutil module
+        try:
+            stats = psutil.net_if_stats()[interface]
+            adapter_info['speed'] = stats.speed
+        except KeyError:
+            adapter_info['speed'] = 'Unknown'
+
+        adapters.append(adapter_info)
+
+    # Create directory if it doesn't exist
+    output_dir = os.path.join(os.environ['LOCALAPPDATA'], 'Skyfay', 'AutoNicConfigurator')
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Save adapters information to JSON file
+    output_file = os.path.join(output_dir, 'network_adapters.json')
+    with open(output_file, 'w') as file:
+        json.dump(adapters, file, indent=4)
