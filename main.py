442a05d2b4bd812f1095f36f28cffd7205e265d9
Manu <skyfay@skymail.one>
2023-06-27 14:56:07 +0200

    added log button to settings and made a toplvlwindow

--- main.py
+++ main.py
@@ -24,6 +24,16 @@ def initialize_adapter_select_placeholder(window):
     window.network_adapter_select.set(placeholder_text)
     window.network_adapter_select.configure(state="readonly")
 
+
+class LogToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(window, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        window.geometry("400x300")
+
+        window.label = customtkinter.CTkLabel(window, text="ToplevelWindow")
+        window.label.pack(padx=20, pady=20)
+
+
 class App(customtkinter.CTk):
     #test
 
@@ -339,6 +349,9 @@ class App(customtkinter.CTk):
         window.settings_pre_frame1 = customtkinter.CTkFrame(window.settings_frame, corner_radius=10, bg_color=("#ebebeb", "#242424"), fg_color=("#f9f9fa", "#343638"), border_width=2, border_color=("#979da2", "#565b5e"))
         window.settings_pre_frame1.grid(row=1, column=0, padx=10, pady=5, sticky="news")
         window.settings_pre_frame1.grid_columnconfigure(0, weight=1)  # 0, weight=1
+        window.settings_pre_frame2 = customtkinter.CTkFrame(window.settings_frame, corner_radius=10, bg_color=("#ebebeb", "#242424"), fg_color=("#f9f9fa", "#343638"), border_width=2, border_color=("#979da2", "#565b5e"))
+        window.settings_pre_frame2.grid(row=2, column=0, padx=10, pady=5, sticky="news")
+        window.settings_pre_frame2.grid_columnconfigure(0, weight=1)  # 0, weight=1
         # Text change the appearance mode
         window.settings_frame_appearance_label = customtkinter.CTkLabel(window.settings_pre_frame, text="Set the App Appearance",
                                                                     font=customtkinter.CTkFont(size=12))
@@ -350,21 +363,39 @@ class App(customtkinter.CTk):
         window.settings_frame_appearance_mode_menu.set(appearance_mode)  # Set the initial value to the loaded appearance_mode
         window.settings_frame_appearance_mode_menu.grid(row=1, column=0, padx=20, pady=10)
 
-        # Text change the appearance mode
+        # Clear the cache
         window.settings_frame_clear_cache_label = customtkinter.CTkLabel(window.settings_pre_frame1, text="In case of problems it may help to clear the cache",
                                                                     font=customtkinter.CTkFont(size=12))
         window.settings_frame_clear_cache_label.grid(row=0, column=0, padx=10, pady=5)
-        # Button to change the appearance mode
+        # Button Clear the cache
         window.settings_frame_clear_cache = customtkinter.CTkButton(window.settings_pre_frame1,
                                                                 text="Clear Cache",
                                                                 command=delete_database_dir)
         window.settings_frame_clear_cache.grid(row=1, column=0, padx=20, pady=10)
+
+        # Open log window
+        window.settings_frame_open_logs_label = customtkinter.CTkLabel(window.settings_pre_frame2, text="Here you can open the logs if you have problems",
+                                                                    font=customtkinter.CTkFont(size=12))
+        window.settings_frame_open_logs_label.grid(row=0, column=0, padx=10, pady=5)
+        # Button to Open log window
+        window.settings_frame_open_logs = customtkinter.CTkButton(window.settings_pre_frame2,
+                                                                text="Open Logs",
+                                                                command=window.open_log_window)
+        window.settings_frame_open_logs.grid(row=1, column=0, padx=20, pady=10)
+
+        window.toplevel_window = None
 
         # select default frame
         window.select_frame_by_name("home")
 
         # select default frame
         window.select_network_by_name("network_information_frame")
+
+    def open_log_window(window):
+        if window.toplevel_window is None or not window.toplevel_window.winfo_exists():
+            window.toplevel_window = LogToplevelWindow(window)  # create window if its None or destroyed
+        else:
+            window.toplevel_window.focus()  # if window exists focus it
 
     # Select Frame "main frames" from navigation frame
     def select_frame_by_name(window, name):
@@ -440,7 +471,6 @@ class App(customtkinter.CTk):
 
     def shortcut_frame_button_event(window):
         window.select_network_by_name("network_shortcut_frame")
-
 
 if __name__ == "__main__":
     app = App()
